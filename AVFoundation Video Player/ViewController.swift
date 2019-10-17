0a664e19aa744844780cebce8f90c69b9261a233
Husseinomda16 <eslamali1133@hotmail.com>
2019-10-17 12:21:31 +0200

    Initial Commit

---
+++ AVFoundation Video Player/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AVFoundation Video Player
+//
+//  Created by Husseinomda16 on 10/17/19.
+//  Copyright © 2019 Hmo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
